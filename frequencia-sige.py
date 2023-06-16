7cedec546ebedd3b9ef1d7928846cdd1454e05d4
martinopole <digita883@gmail.com>
2023-06-16 13:39:02 -0300

    Alterando webdriver

--- frequencia-sige.py
+++ frequencia-sige.py
@@ -3,5 +3,5 @@ from selenium import webdriver
 usuario = input("Qual o usuário do SIGE? ")
 senha = input("Qual a senha? ")
 
-driver = webdriver.Firefox()
-driver.get("http://http://sige.seduc.ce.gov.br/")
+driver = webdriver.Chrome()
+driver.get("http://sige.seduc.ce.gov.br/")
